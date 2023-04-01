f874d577c6c9f1fdcd2aaabc22a44496917e0ebf
Gracjan Chodorski <chodorski.gracjan@gmail.com>
2023-04-01 18:18:07 +0200

    Zadanie 1

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Gracjan");
+        System.out.println("23");
+        System.out.println("C");
+    }
+}
