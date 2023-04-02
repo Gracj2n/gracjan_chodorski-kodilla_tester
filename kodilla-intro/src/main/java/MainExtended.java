abd5d8189cb49d0c313f967922930dc8aa456004
Gracjan Chodorski <chodorski.gracjan@gmail.com>
2023-04-02 13:58:00 +0200

    Zmienne 1.1

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,17 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String myName = "Gracjan";
+        char firstLetterLastName = 'C';
+        boolean duringCourse = true;
+        int myWeightKg = 88;
+        double myHeightM = 1.90;
+        String exampleText = "Bootcamp TA 2023-04";
+        System.out.println(myName);
+        System.out.println(firstLetterLastName);
+        System.out.println(duringCourse);
+        System.out.println(myWeightKg);
+        System.out.println(myHeightM);
+        System.out.println(exampleText);
+
+    }
+}
