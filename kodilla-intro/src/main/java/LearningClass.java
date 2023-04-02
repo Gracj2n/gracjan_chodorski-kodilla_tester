da86de9e352cdf42537414678afd53a1860ba823
Gracjan Chodorski <chodorski.gracjan@gmail.com>
2023-04-02 14:53:35 +0200

    a

--- kodilla-intro/src/main/java/LearningClass.java
+++
@@ -1,8 +0,0 @@
-public class LearningClass {
-    public static main(String[] args) {
-        int sum(int a, int b) {
-            int result = a + b;
-            return result;
-            System.out.println(result);
-    }
-}
