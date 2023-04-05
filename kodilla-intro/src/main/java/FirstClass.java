9b11f0b3dcd6bdd38ee4c01843a6a534cd8dc3b8
Gracjan Chodorski <chodorski.gracjan@gmail.com>
2023-04-05 18:28:31 +0200

    RandomNumbers

--- kodilla-intro/src/main/java/FirstClass.java
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -1,6 +1,7 @@
-public class FirstClass {                             // [1]
-    public static void main(String[] args) {          // [2]
-
-        System.out.println("Hello from FirstClass!"); // [3]
+public class FirstClass {
+    public static void main(String[] args) {
+        RandomNumbers randomNumbers = new RandomNumbers();
+        int wynik = randomNumbers.getCountOfRandomNumber(10);
+        System.out.println(wynik);
     }
 }
