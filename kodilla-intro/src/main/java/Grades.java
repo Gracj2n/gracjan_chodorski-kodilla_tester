5cc52e9349414f0c17fcfa15a9429476ad3331c6
Gracjan Chodorski <chodorski.gracjan@gmail.com>
2023-04-16 22:34:37 +0200

    Zadanie z ocenami (wymaga korekty w kwestii wywietlania sredniej ocen)

--- kodilla-intro/src/main/java/Grades.java
+++ kodilla-intro/src/main/java/Grades.java
@@ -1,28 +1,45 @@
 public class Grades {
-
-
-    int[] grades;  /* co to i po co taka zmienna? i czemu ma być private */
-    int size ;
-    int grade;
-
-
-    public Grades(int grade) { /* konstruktor */
-        this.grades = new int[10];  /* to jest stworzona tablica? */
-        this.size = 0;              /* aktualne miejsce w tablicy? */
-        this.grade = grade;
-
-        for (int g = grades.length -1; g >= 0; g--) {
-            System.out.println(grades[g]);             /* wyświetlanie aktualnej tablicy */
-        }
-
+    private int[] grades;  // co to i po co taka zmienna? i czemu ma być private
+    private int size;
+    private int gradeValue;
+    private int sumOfGrades;
+    public Grades() {              // konstruktor
+        this.grades = new int[10];              //  to jest stworzona tablica?
+        this.size = 3; /* aktualne miejsce w tablicy? */
     }
 
     public void add(int gradeValue) {
+        this.gradeValue = gradeValue;
         if (this.size == 10) {
-          return;                 /* przerywanie działania */
-      }
+            return;                 // przerywanie działania
+        }
         this.grades[this.size] = gradeValue;
         this.size++;
     }
+
+    public void showGrade() {
+        System.out.println("Dodano ocenę " + this.gradeValue + ".");
+    }
+
+    public void averageOfGrades() {
+        int[] oceny = new int[] {1, 3, 4, 5, 6, 3};
+        double sumOfGrades = 0;
+        for (int a = 0; a < oceny.length; a++) {
+            sumOfGrades += oceny[a];
+        }
+        double average = sumOfGrades / oceny.length;
+        System.out.println(average);
+    }
+
+
+
+    //Pokwazywanie Tablicy
+    public void showGrades() {
+        for (int g = 0; g < grades.length; g++) {
+            System.out.println("Ocena:" + grades[g]);
+            }
+        }
+
+
 }
 
--- kodilla-intro/src/main/java/GradesLaunch.java
+++ kodilla-intro/src/main/java/GradesLaunch.java
@@ -1,7 +1,26 @@
 public class GradesLaunch {
     public static void main(String[] args) {
-        Grades grade = new Grades(3);
-        System.out.println("Dodaj ocenę " + grade.grade + " do tablicy.");
 
+        Grades grade1 = new Grades();
+        grade1.add(3);
+        grade1.showGrade();
+
+        Grades grade2 = new Grades();
+        grade2.add(2);
+        grade2.showGrade();
+
+        Grades grade3 = new Grades();
+        grade3.add(4);
+        grade3.showGrade();
+
+        Grades grade4 = new Grades();
+        grade4.add(1);
+        grade4.showGrade();
+
+        System.out.println("");
+
+        Grades averageOfGrades = new Grades();
+        averageOfGrades.averageOfGrades();
     }
+
 }
