2e204463315e044c4dd0e06f5702960355afe179
Gracjan Chodorski <chodorski.gracjan@gmail.com>
2023-04-17 14:43:20 +0200

    Zadanie: zmienne i metody statystyczne

---
+++ kodilla-intro/src/main/java/Book.java
@@ -0,0 +1,11 @@
+public class Book {
+    private String author;
+    private String title;
+
+    public static Book of(String author, String title) {
+        System.out.println(author + " " + title);
+        return null;
+    }
+
+
+}
---
+++ kodilla-intro/src/main/java/BookLaunch.java
@@ -0,0 +1,5 @@
+public class BookLaunch {
+    public static void main(String[] args) {
+        Book book1 = Book.of("Adam Mickiewicz","Pan Tadeusz");
+    }
+}
