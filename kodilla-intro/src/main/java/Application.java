636439f5dcfad12d14ee1c98e200519e055b3f0f
Gracjan Chodorski <chodorski.gracjan@gmail.com>
2023-04-17 19:34:09 +0200

    Application opis dzialan

--- kodilla-intro/src/main/java/Application.java
+++ kodilla-intro/src/main/java/Application.java
@@ -3,13 +3,13 @@ public class Application {
     double age;
     double height;
 
-    public Application(String name, double age, double height) {
+    public Application(String name, double age, double height) { //konstruktor
         this.name = name;
         this.age = age;
         this.height = height;
     }
 
-    public void checkAgeAndHeight() {
+    public void checkAgeAndHeight() { //metoda chechAgeAndHeight
         if (name != null) {
             if (age > 30 && height > 160) {
                 System.out.println("User is older than 30 and taller than 160cm");
--- kodilla-intro/src/main/java/ApplicationLaunch.java
+++ kodilla-intro/src/main/java/ApplicationLaunch.java
@@ -1,9 +1,9 @@
 import java.sql.SQLOutput;
 
 public class ApplicationLaunch {
-    public static void main(String[] args) {
-        Application person = new Application("Adam", 10.5,120);
+    public static void main(String[] args) {                                     //metoda main
+        Application person = new Application("Adam", 10.5,120); //obiekt person
         System.out.println(person.name);
-        person.checkAgeAndHeight();
+        person.checkAgeAndHeight();             //wywołanie metody checAgeAndHeight z klasy Application
     }
 }
