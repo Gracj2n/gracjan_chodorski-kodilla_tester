452d806dc3c102f8e58c645e8fc25f88c268e652
Gracjan Chodorski <chodorski.gracjan@gmail.com>
2023-04-26 19:19:19 +0200

    Zadanie JUnit testy

--- kodilla-basic-tests/src/test/java/com/kodilla/basic_asseration/CalculatorTestSuite.java
+++ kodilla-basic-tests/src/test/java/com/kodilla/basic_asseration/CalculatorTestSuite.java
@@ -14,19 +14,35 @@ public class CalculatorTestSuite {
         int b = 8;
         int sumResult = testSum.sum(a, b);
         assertEquals(13, sumResult, 0.1);
-
+    }
+    @Test
+    public void testSubstract() {
         Calculator testSubstract = new Calculator();
         int a1 = 5;
         int b1 = 7;
         int substractResult = testSubstract.substract(a1, b1);
         assertEquals(-2, substractResult, 0.1);
+    }
+    @Test
+    public void testSquared0() {
+        Calculator testSquared = new Calculator();
+        int a2 = 0;
+        int sumSquared = testSquared.squared(a2);
+        assertEquals(25, sumSquared, 0.1);
 
+    }
+    @Test
+    public void testSquared1() {
         Calculator testSquared = new Calculator();
         int a2 = -5;
         int sumSquared = testSquared.squared(a2);
-        assertEquals(-25, sumSquared, 0.1);
-        assertEquals(0, sumSquared, 0.1);
         assertEquals(25, sumSquared, 0.1);
-
+    }
+    @Test
+    public void testSquared2() {
+        Calculator testSquared = new Calculator();
+        int a2 = 5;
+        int sumSquared = testSquared.squared(a2);
+        assertEquals(25, sumSquared, 0.1);
     }
 }
