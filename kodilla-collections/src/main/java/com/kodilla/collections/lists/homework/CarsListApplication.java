faf648be6446af8a92012e00175d4b9f6e8e6dda
Gracjan Chodorski <chodorski.gracjan@gmail.com>
2023-05-03 16:20:08 +0200

    Zadanie: operacje na listach

---
+++ kodilla-collections/src/main/java/com/kodilla/collections/lists/homework/CarsListApplication.java
@@ -0,0 +1,29 @@
+package com.kodilla.collections.lists.homework;
+
+import com.kodilla.collections.arrays.homework.CarUtils;
+import com.kodilla.collections.interfaces.homework.Car;
+import com.kodilla.collections.interfaces.homework.Ford;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarsListApplication {
+    public static void main(String[] args) {
+        List<Ford> cars = new ArrayList<>();
+
+        Ford ford1 = new Ford();
+
+        cars.add(ford1);
+        cars.add(new Ford());
+        cars.add(new Ford());
+
+        cars.remove(ford1);
+        cars.remove(0);
+
+        System.out.println("Rozmiar kolekcji: " + cars.size());
+
+        for(Ford car : cars) {
+            System.out.println("Prędkość: " + car.getSpeed());
+        }
+    }
+}
