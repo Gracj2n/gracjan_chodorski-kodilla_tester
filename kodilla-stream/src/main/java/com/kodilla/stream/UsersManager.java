4ec2887936446f58977cdffc46b5eec0af57ef55
Gracjan Chodorski <chodorski.gracjan@gmail.com>
2023-05-14 18:07:39 +0200

    Zad z wiekiem

--- kodilla-stream/src/main/java/com/kodilla/stream/UsersManager.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/UsersManager.java
@@ -1,18 +1,42 @@
 package com.kodilla.stream;
 
-public class UserManager {
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class UsersManager {
     public static void main(String[] args) {
-        processUsersStream();
+        List<String> chemistsUsernames = filterChemistGroupUsernames();
+        System.out.println("Chemists: " + chemistsUsernames);
+
+        System.out.println();
+
+        List<Integer> usersOver35yo = filterUsersOver35();
+        System.out.println("Users over 35 years old: " + usersOver35yo);
+
+
     }
 
-    private static void processUsersStream() {
-        UsersRepository.getUsersList()
+    public static List<String> filterChemistGroupUsernames() {
+        List<String> usernames = UsersRepository.getUsersList()
                 .stream()
-                .map(user -> user.getUsername())
-                .forEach(username -> System.out.println(username));
+                .filter(user -> user.getGroup().equals("Chemists"))
+                .map(UsersManager::getUserName)
+                .collect(Collectors.toList());
+        return usernames;
     }
 
+    public static List<Integer> filterUsersOver35() {
+        List<Integer> age = UsersRepository.getUsersList()
+                .stream()
+                .filter(user -> user.getAge() > 35)
+                .map(UsersManager::getUserAge)
+                .collect(Collectors.toList());
+                return age;
+    }
     public static String getUserName(User user) {
         return user.getUsername();
+    }
+    public static int getUserAge(User userAge) {
+        return userAge.getAge();
     }
 }
---
+++ kodilla-stream/src/test/java/com/kodilla/stream/UsersManagerTest.java
@@ -0,0 +1,43 @@
+package com.kodilla.stream;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UsersManagerTest {
+
+    @Test
+    public void doesReturnUsernames() {
+        //given
+        UsersManager usersManager = new UsersManager();
+        //when
+        List<String> userNames = usersManager.filterChemistGroupUsernames();
+        //then
+        List<String> expectedNames = new ArrayList<>();
+        expectedNames.add(new String("Walter White"));
+        expectedNames.add(new String("Gale Boetticher"));
+        assertEquals(expectedNames, userNames);
+    }
+
+    @Test
+    public void doesFilterUsersOver35() {
+        //given
+        UsersManager ageMethod = new UsersManager();
+        //when
+        List<Integer> userNamesOver35 = ageMethod.filterUsersOver35();
+        //then
+        List<Integer> expectedUsersAge = new ArrayList<>();
+        expectedUsersAge.add(50);
+        expectedUsersAge.add(49);
+        expectedUsersAge.add(44);
+        expectedUsersAge.add(57);
+        assertEquals(expectedUsersAge, userNamesOver35);
+
+
+    }
+}
