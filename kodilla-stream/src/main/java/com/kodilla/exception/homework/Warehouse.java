0e6217b8a733469db4fc3728719951442d92345a
Gracjan Chodorski <chodorski.gracjan@gmail.com>
2023-05-23 19:20:43 +0200

    Zadanie: obsluga bledow

---
+++ kodilla-stream/src/main/java/com/kodilla/exception/homework/Warehouse.java
@@ -0,0 +1,9 @@
+package com.kodilla.exception.homework;
+
+public class Warehouse {
+    public void addOrder(Order order) {}
+    public Order getOrder(String number) {
+        Order orders = new Order("1");
+        return orders;
+    }
+}
---
+++ kodilla-stream/src/main/java/com/kodilla/exception/homework/WarehouseApp.java
@@ -0,0 +1,14 @@
+package com.kodilla.exception.homework;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WarehouseApp {
+    public static void main(String[] args) {
+        Warehouse warehouse = new Warehouse();
+
+        List<Order> orders = new ArrayList<>();
+
+        orders.add(new Order("1"));
+    }
+}
