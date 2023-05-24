003a0780a8aea99040a252107086e9f3305c8c76
Gracjan Chodorski <chodorski.gracjan@gmail.com>
2023-05-24 09:40:36 +0200

    String sources

--- Kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/StringManipulatorTestSuite.java
+++ Kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/StringManipulatorTestSuite.java
@@ -1,14 +1,26 @@
 package com.kodilla.parametrized_tests;
 
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class StringManipulatorTestSuite {
     private StringManipulator stringManipulator = new StringManipulator();
-
+    private static Stream<Arguments> provideStringsForTestingLength() {
+        return Stream.of(
+                Arguments.of("test", 4),
+                Arguments.of("OtHEr ", 5),
+                Arguments.of("E V e n t", 5),
+                Arguments.of("null ", 4),
+                Arguments.of("A", 1)
+        );
+    }
     @ParameterizedTest
     @CsvFileSource(resources = "/stringWithLowerCase.csv", numLinesToSkip = 1)
     public void shouldReverseStringWithLowerCase(String input, String expected) {
@@ -16,8 +28,13 @@ class StringManipulatorTestSuite {
     }
 
     @ParameterizedTest
-    @CsvSource(value = {"test,4", " OtHEr ,5", "E V e n t,5", "null ,4", "A,1"})
+    @MethodSource(value = "provideStringsForTestingLength")
     public void shouldCalculateStringLengthWithoutSpaces(String input, int expected) {
+        assertEquals(expected, stringManipulator.getStringLengthWithoutSpaces(input));
+    }
+    @ParameterizedTest
+    @MethodSource(value = "com.kodilla.parametrized_tests.StringSources#provideStringsForTestingLength")
+    public void shouldCalculateStringLengthWithoutSpacesV2(String input, int expected) {
         assertEquals(expected, stringManipulator.getStringLengthWithoutSpaces(input));
     }
     @ParameterizedTest
