bd9d37ad904ea680d2a6a2628e3348d29fd9d137
Gracjan Chodorski <chodorski.gracjan@gmail.com>
2023-06-11 15:41:39 +0200

    Calculator spring

---
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_dependency_injection/homework/Calculator.java
@@ -0,0 +1,27 @@
+package com.kodilla.spring.basic.spring_dependency_injection.homework;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Calculator {
+    private Display display;
+
+    public Calculator(Display display) {    //wstrzykiwanie przez konstruktor
+        this.display = display;
+    }
+
+    public double add(double a, double b) {
+        return a + b;
+    }
+    public double subtract(double a, double b) {
+        return a - b;
+    }
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+    public double divide(double a, double b) {
+        return a / b;
+    }
+
+
+}
---
+++ kodilla-spring-basic/src/test/java/com/kodilla/spring/basic/spring_dependency_injection/homework/CalculatorTestSuite.java
@@ -0,0 +1,37 @@
+package com.kodilla.spring.basic.spring_dependency_injection.homework;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTestSuite {
+
+    private ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.basic");
+    private Calculator bean = context.getBean(Calculator.class);
+    @Test
+    public void shouldAdd() {
+        double numbersAdd = bean.add(10, 5);
+        Assertions.assertEquals(15, numbersAdd);
+    }
+
+    @Test
+    public void shouldSubtract() {
+        double numbersSubstract = bean.subtract(10, 5);
+        Assertions.assertEquals(5, numbersSubstract);
+    }
+
+    @Test
+    public void shouldMultiply() {
+        double numbersMultiply = bean.multiply(10, 5);
+        Assertions.assertEquals(50, numbersMultiply);
+    }
+
+    @Test
+    public void shouldDivide() {
+        double numbersDivide = bean.divide(10, 5);
+        Assertions.assertEquals(2, numbersDivide);
+    }
+}
