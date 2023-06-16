3932c2a6312e2861edd5e7464bdb7d7dfb371aba
Gracjan Chodorski <chodorski.gracjan@gmail.com>
2023-06-16 21:20:42 +0200

    shop tests

--- Kodilla-advanced-tests/src/test/java/com/kodilla/execution_model/homework/ShopTestSuite.java
+++ Kodilla-advanced-tests/src/test/java/com/kodilla/execution_model/homework/ShopTestSuite.java
@@ -2,21 +2,16 @@ package com.kodilla.execution_model.homework;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
 import java.time.Instant;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-
-import static java.time.Instant.parse;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ShopTestSuite {
 
+    Shop shop = new Shop();
+
     @Test
     public void shouldAddOrder() {
-        Shop shop = new Shop();
-
         Order order = new Order(1150, Instant.now(), "exampleLogin");
 
         shop.addOrder(order);
@@ -26,8 +21,6 @@ class ShopTestSuite {
 
     @Test
     public void shouldReturnOrderFromToDate() {
-        Shop shop = new Shop();
-
         Order order1 = new Order(1150, Instant.parse("2020-06-16T18:06:00.00Z"), "exampleLogin");
         Order order2 = new Order(1150, Instant.parse("2023-06-16T18:06:00.00Z"), "exampleLogin");
         Order order3 = new Order(1150, Instant.parse("2022-06-16T18:06:00.00Z"), "exampleLogin");
@@ -38,12 +31,11 @@ class ShopTestSuite {
         shop.addOrder(order3);
         shop.addOrder(order4);
 
-        List<Order> orderList = List.of(order2, order3, order4);
-
+        List<Order> actualList = List.of(order2, order3, order4);
         List<Order> expectedList = shop.getOrderListFromToDate(Instant.parse("2022-01-01T00:00:00.00Z"), Instant.parse("2024-01-01T00:00:00.00Z"));
 
         Assertions.assertEquals(expectedList.size(), 3);
-        Assertions.assertEquals(expectedList, orderList);
+        Assertions.assertEquals(expectedList, actualList);
     }
 
     @Test
@@ -61,14 +53,14 @@ class ShopTestSuite {
         shop.addOrder(order4);
 
         List<Order> expectedList = shop.getOrderByValue(350, 3000);
+        List<Order> actualList = List.of(order2, order3);
 
         Assertions.assertEquals(expectedList.size(), 2);
-
+        Assertions.assertEquals(expectedList, actualList);
     }
+
     @Test
     public void shouldReturnSumValueOfOrders() {
-        Shop shop = new Shop();
-
         Order order1 = new Order(1150, Instant.now(), "exampleLogin");
         Order order2 = new Order(3500, Instant.now(), "exampleLogin");
         Order order3 = new Order(1100, Instant.now(), "exampleLogin");
@@ -78,8 +70,6 @@ class ShopTestSuite {
         shop.addOrder(order2);
         shop.addOrder(order3);
         shop.addOrder(order4);
-
-        shop.sumValueOfOrders();
 
         assertEquals(12700, shop.sumValueOfOrders());
     }
