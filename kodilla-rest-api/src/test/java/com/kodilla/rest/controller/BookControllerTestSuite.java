ab0f2a08fac5f596262e0ba183a5f34ded85b98d
Gracjan Chodorski <chodorski.gracjan@gmail.com>
2023-06-28 18:16:05 +0200

    Book controller

--- kodilla-rest-api/src/test/java/com/kodilla/rest/controller/BookControllerTestSuite.java
+++ kodilla-rest-api/src/test/java/com/kodilla/rest/controller/BookControllerTestSuite.java
@@ -4,8 +4,10 @@ import com.kodilla.rest.domain.BookDto;
 import com.kodilla.rest.service.BookService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 
+import java.awt.print.Book;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -31,5 +33,30 @@ class BookControllerTestSuite {
 
         //then
         assertThat(result).hasSize(2);
+    }
+
+    @Test
+    public void shouldAddBook() {
+        //given
+        //BookService booksServiceMock = Mockito.mock(BookService.class);
+        BookController bookController = Mockito.mock(BookController.class);
+        //BookController bookController = new BookController(booksServiceMock);
+        BookDto book1 = new BookDto("Title 1", "Author 1");
+        //BookDto book2 = new BookDto("Title 2", "Author 2");
+        //when
+        bookController.addBook(book1);
+        //then
+        Mockito.verify(bookController, Mockito.atMostOnce()).addBook(book1);
+    }
+
+    @Test
+    public void shouldServiceAddBook() {
+        //given
+        BookService bookService = Mockito.mock(BookService.class);
+        BookDto book = new BookDto("Title 1", "Author 1");
+        //when
+        bookService.addBook(book);
+        //then
+        Mockito.verify(bookService, Mockito.times(1)).addBook(book);
     }
 }
