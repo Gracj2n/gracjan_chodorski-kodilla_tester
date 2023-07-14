c37d55e5bae904afe16ff84ac850d0182a89607b
Gracjan Chodorski <chodorski.gracjan@gmail.com>
2023-07-14 11:47:40 +0200

    123123123

--- kodilla-login-selenium/src/main/java/com/kodilla/selenium/pom/KodillaLoginPom.java
+++ kodilla-login-selenium/src/main/java/com/kodilla/selenium/pom/KodillaLoginPom.java
@@ -1,34 +1,25 @@
 package com.kodilla.selenium.pom;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
 
 public class KodillaLoginPom {
 
-    @FindBy(css = "input[type='email']")
-    WebElement emailField;
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "/Users/gracjanchodorski/Desktop/gracjan_chodorski-kodilla_tester/chromedriver_mac64/chromedriver");
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--remote-allow-origins=*");
+        WebDriver webDriver = new ChromeDriver(chromeOptions);
+        webDriver.get("https://kodilla.com/pl/test/login");
 
-    @FindBy(css = "input[type='password']")
-    WebElement passwordField;
+        WebElement emailField = webDriver.findElement(By.xpath("//div[@class=\"form-group\"]/input[@id=\"email\"]"));
+        WebElement passwordField = webDriver.findElement(By.xpath("//div[@class=\"form-group\"]/input[@id=\"password\"]"));
 
-    @FindBy(xpath = "//button[contains(text(), 'Log in')]")
-    WebElement loginButton;
-
-    WebDriver driver;
-
-    public KodillaLoginPom(WebDriver driver) {
-        this.driver = driver;
-        PageFactory.initElements(driver, this);
-    }
-
-    public boolean login(String email, String password) {
-        emailField.sendKeys(email);
-        passwordField.sendKeys(password);
-        loginButton.click();
-        String message = driver.switchTo().alert().getText();
-        driver.switchTo().alert().dismiss();
-        return message.equals("Jesteś teraz zalogowany!");
+        emailField.sendKeys("test1234@mail.com");
+        passwordField.sendKeys("123123123");
     }
 }
