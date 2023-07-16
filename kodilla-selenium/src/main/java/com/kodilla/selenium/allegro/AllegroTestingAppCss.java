38e6322dec206b7e2d05910d561437fc5440db48
Gracjan Chodorski <chodorski.gracjan@gmail.com>
2023-07-16 17:22:01 +0200

    allegro product list

--- kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingAppCss.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingAppCss.java
@@ -5,14 +5,18 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 
 public class AllegroTestingAppCss {
+
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "/Users/gracjanchodorski/Data/chromedriver_mac64/chromedriver");
 
@@ -21,29 +25,28 @@ public class AllegroTestingAppCss {
         WebDriver webDriver = new ChromeDriver(chromeOptions);
         webDriver.get("https://allegro.pl");
 
-        WebElement alertAccept = webDriver.findElement(By.xpath("//button[@data-testid=\"accept_home_view_action\"]"));
+        //Amazon.pl
+        //webDriver.get("https://amazon.pl");
+        //WebElement alertAccept = webDriver.findElement(By.id("sp-cc-accept"));
+        //WebElement searchField = webDriver.findElement(By.id("twotabsearchtextbox"));
+
+        WebElement alertAccept = webDriver.findElement(By.xpath("//button[@data-testid='accept_home_view_action']"));
         alertAccept.click();
 
-
-        //WebElement searchField = webDriver.findElement(By.xpath("//*[@placeholder=\"czego szukasz?\"]"));
-        //WebElement searchField = webDriver.findElement(By.cssSelector("//form[@data-role='search-form']"));
-        WebElement searchField = webDriver.findElement(By.tagName("._535b5_Y5CDc"));
-        //WebElement searchField = webDriver.findElement(By.className("msts_pt mgmw_wo mr3m_1 mli2_1 mjyo_6x mse2_40 mqu1_40 mp4t_0 m3h2_0 mryx_0 munh_0 mg9e_0 mj7a_0 mh36_0 mvrt_8 mlkp_6x mefy_5r mldj_0 mm2b_0 ozg4u mh85_0 _535b5_Y5CDc"));
-        searchField.click();
+        WebElement searchField = webDriver.findElement(By.xpath("//input[@data-role='search-input']"));
         searchField.sendKeys("Mavic mini");
-        //searchField.submit();
 
-        //WebDriverWait wait = new WebDriverWait(webDriver, 5);
-        //wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//div/form[@data-role=\"search-form")));
+        WebElement firstProduct = webDriver.findElement(By.xpath("//a[@href=\"/listing?string=dji%20mavic%20mini\"]"));
+        WebElement secondProduct = webDriver.findElement(By.xpath("//a[@href=\"/listing?string=dji%20mavic%20mini%202\"]"));
 
+        List<WebElement> productList = new ArrayList<>();
+        productList.add(firstProduct);
+        productList.add(secondProduct);
 
-
-        //List<WebElement> mavicList = webDriver.findElements(By.className("mp0t_0a mgmw_3z mgn2_14 mqu1_21"));;
-        //mavicList.get(1);
-
-        //List<WebElement> mavicList = webDriver.findElements(By.xpath("//div[@class=\"mqu1_1\"]"));
-        //mavicList.get(0).findElement(By.className("mqu1_1")).click();
-
+        System.out.println(firstProduct.getText());
+        System.out.println(secondProduct.getText());
+        System.out.println(productList.get(0).getText());
+        System.out.println(productList.get(1).getText());
 
     }
 }
