b8cca940295e55ed9caa985468fa62eb4ed3a745
Gracjan Chodorski <chodorski.gracjan@gmail.com>
2023-07-24 20:04:43 +0200

    ocular tests

---
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/visual/OcularExample.java
@@ -0,0 +1,28 @@
+package com.kodilla.selenium.visual;
+
+import com.testautomationguru.ocular.Ocular;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.nio.file.Paths;
+
+public class OcularExample {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "/Users/gracjanchodorski/Desktop/gracjan_chodorski-kodilla_tester/chromedriver_mac64/chromedriver");
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--remote-allow-origins=*");
+        WebDriver webDriver = new ChromeDriver(chromeOptions);
+
+        Ocular.config()
+                .resultPath(Paths.get("/Users/gracjanchodorski/Desktop/gracjan_chodorski-kodilla_tester/Ocular/results"))
+                .snapshotPath(Paths.get("/Users/gracjanchodorski/Desktop/gracjan_chodorski-kodilla_tester/Ocular/snapshots"))
+                .globalSimilarity(95)
+                .saveSnapshot(true);
+
+        WorldTimePage page = new WorldTimePage(webDriver);
+        page.open();
+        page.compare();
+        page.close();
+    }
+}
